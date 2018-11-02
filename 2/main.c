679097e031a1c236fdc1974fd2650c88c1e77af5
Roman Khabibov <roman.habibov1@yandex.ru>
2018-11-03 02:59:36 +0300

    Redo.

--- 2/main.c
+++ 2/main.c
@@ -6,6 +6,58 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+struct simple_cmd {
+	size_t _num_of_args;
+	char **_cmd_args;
+};
+
+struct simple_cmd *
+alloc_simple_cmd(size_t length)
+{
+	struct simple_cmd *ret = malloc(sizeof(struct simple_cmd));
+	ret->_cmd_args = malloc(sizeof(char *) * length);
+	return ret;
+}
+
+struct simple_cmd *
+realloc_simple_cmd(struct simple_cmd *cmd, size_t length)
+{
+	cmd->_cmd_args = realloc(cmd->_cmd_args, sizeof(char *) * length);
+	return cmd;
+}
+
+struct full_cmd {
+	size_t _num_of_simple_cmds;
+	char *_input_file;
+	char *_output_file;
+	struct simple_cmd *_simple_cmds;
+	int _background;
+};
+
+char*
+get_str(char* line) {
+	size_t size = 64;
+	char* res = malloc(sizeof(char) * size);
+	assert(res);
+
+	size_t i = 0;
+	while(((*line) != '\0') ||
+		  ((*line) != ' ')) {
+		res[i] = *line;
+		i++;
+		line++;
+		if (i == size) {
+			size += 64;
+			res = realloc(res, sizeof(char) * size);
+			assert(res);
+		}
+	}
+	res[i] = '\0';
+	line++;
+
+	return res;
+}
+
 char *
 read_line() {
 	size_t bufsize = 1024;
@@ -29,65 +81,152 @@ read_line() {
 	return str_buf;
 }
 
-char **
-get_command(char *line) {
-	size_t tokens_size = 64;
-	char **tokens = malloc(tokens_size * sizeof(char*));
-	assert(tokens);
+struct simple_cmd *
+get_simple_cmd(char *line) {
+	struct simple_cmd *res;
+	size_t args_size = 64;
+	res = alloc_simple_cmd(args_size);
+	assert(res);
 
 	size_t i = 0;
-	size_t number_of_tokens = 0;
-	while ((line[i] == '>') || (line[i] == '<') || (line[i] == '|')) {
-		size_t token_i = 0;
-		size_t token_size = 64;
-		char *token = malloc(sizeof(char) * token_size);
-		assert(token);
+	size_t num_of_args = 0;
+	while ((line[i] != '|') ||
+		   (line[i] != '<') ||
+		   (line[i] != '>') ||
+		   (line[i] != '\0')) {
+		size_t arg_i = 0;
+		size_t arg_size = 64;
+		char *arg = malloc(sizeof(char) * arg_size);
+		assert(arg);
 		while (line[i] != ' ') {
-			token[token_i] = line[i];
-			token_i++;
+			arg[arg_i] = line[i];
+			arg_i++;
 			i++;
 
-			if (token_i == token_size) {
-				token_size += 64;
-				token = realloc(token,sizeof(char) * token_size);
-				assert(token);
+			if (arg_i == arg_size) {
+				arg_size += 64;
+				arg = realloc(arg, sizeof(char) * arg_size);
+				assert(arg);
 			}
 		}
-		token[token_i] = '\0';
+		arg[arg_i] = '\0';
 
-		number_of_tokens++;
-		if (tokens_size == number_of_tokens) {
-			tokens_size += 64;
-			tokens = realloc(tokens,sizeof(char*) * tokens_size);
-			assert(token);
+		num_of_args++;
+		if (args_size == num_of_args) {
+			args_size += 64;
+			res = realloc_simple_cmd(res, args_size);
+			assert(res);
 		}
-		tokens[number_of_tokens - 1] = token;
+		res->_cmd_args[num_of_args - 1] = arg;
 		i++;
 	}
-	tokens[number_of_tokens] = NULL;
+	res->_cmd_args[num_of_args] = NULL;
+	res->_num_of_args = num_of_args;
 
 	line += i;
 
-	return tokens;
+	return res;
+}
+
+struct full_cmd *
+get_full_cmd(char *line) {
+	struct full_cmd *res = malloc(sizeof(struct full_cmd));
+	assert(res);
+
+	size_t simple_cmds_size = 4;
+	struct simple_cmd *simple_cmds = malloc(sizeof(struct simple_cmd) * simple_cmds_size);
+	assert(simple_cmds);
+
+	size_t simple_cmd_i = 0;
+	do {
+		simple_cmds[simple_cmd_i] = get_simple_cmd(line);
+		simple_cmd_i++;
+		if (simple_cmd_i == simple_cmds_size) {
+			simple_cmds_size += 4;
+			simple_cmds = realloc(simple_cmds, sizeof(struct simple_cmd) * simple_cmds_size);
+			assert(simple_cmds);
+		}
+		if ((*line) == '|') {
+			line += 2;
+			continue;
+		}
+	} while (((*line) != '\0') ||
+			 ((*line) != '<') ||
+			 ((*line) != '>'));
+	simple_cmds[simple_cmd_i] = NULL;
+	res->_simple_cmds = simple_cmds;
+	res->_num_of_simple_cmds = simple_cmd_i;
+
+	if ((*line) == '>') {
+		line++;
+		if ((*line) == '>') {
+			line++;
+		}
+		line++;
+
+		res->_input_file = get_str(line);
+	}
+
+	if ((*line) == '<') {
+		line++;
+		if ((*line) == '<') {
+			line++;
+		}
+		line++;
+
+		res->_input_file = get_str(line);
+	}
+
+	return res;
 }
 
 int
-launch(char **args) {
-	pid_t pid, wpid;
-	int status;
+launch(struct full_cmd *full_cmd) {
+	int tmp_in = dup(0);
+	int tmp_out dup(1);
 
-	pid = fork();
-	if (pid == 0) {
-		if (execvp(args[0], args) == -1) {
-			perror("Error");
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		perror("Error");
+	int fdin;
+	int ret;
+	if(full_cmd->_input_file) {
+		fdin = open(full_cmd->_input_file, O_RONLY);
 	} else
-		do {
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		fdin = dup(tmp_in);
+
+	int fdout;
+	for (size_t i = 0; i < full_cmd->_num_of_simple_cmds; i++) {
+		dup2(fdin, 0);
+		close(fdin);
+		if ((full_cmd->_num_of_simple_cmds - 1) == i) {
+			if (full_cmd->_output_file)
+				fdout = open(full_cmd->_output_file, O_CREAT | O_WRONLY);
+			else
+				fdout = dup(tmp_out);
+		} else {
+			int fdpipe[2];
+			pipe(fdpipe);
+			fdout = fdpipe[1];
+			fdin = fdpipe[0];
+		}
+
+		dup2(fdout, 1);
+		close(fdout);
+
+		ret = fork();
+		if (!ret) {
+			execvp(full_cmd->_simple_cmds->_cmd_args[0],
+				   full_cmd->_simple_cmds->_cmd_args);
+			perror("execvp");
+			exit(1);
+		}
+	}
+
+	dup2(tmp_in, 0);
+	dup2(tmp_out, 1);
+	close(tmp_in);
+	close(tmp_out);
+
+	if (!full_cmd->_background)
+		waitpid(ret, NULL);
 
 	return 1;
 }
