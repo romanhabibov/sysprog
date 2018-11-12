964ec14c0ee9714bc2355e1f28e1749572d14782
Roman Khabibov <roman.habibov1@yandex.ru>
2018-11-12 14:39:05 +0300

    Fix.

--- 2/main.c
+++ 2/main.c
@@ -6,6 +6,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <assert.h>
+#include <stdbool.h>
+#include "shell.h"
+
+size_t number_of_builtins = 3;
+
+char *line = NULL;
 
 struct simple_cmd {
 	size_t _num_of_args;
@@ -29,10 +35,12 @@ realloc_simple_cmd(struct simple_cmd *cmd, size_t length)
 
 struct full_cmd {
 	size_t _num_of_simple_cmds;
+	bool _input_flag;
+	bool _output_flag;
+	int _background;
 	char *_input_file;
 	char *_output_file;
 	struct simple_cmd *_simple_cmds;
-	int _background;
 };
 
 void free_simple_cmd(struct simple_cmd *simple_cmd) {
@@ -51,14 +59,13 @@ void free_full_cmd(struct full_cmd *full_cmd) {
 }
 
 char*
-get_str(char* line) {
+get_str() {
 	size_t size = 64;
 	char* res = malloc(sizeof(char) * size);
 	assert(res);
 
 	size_t i = 0;
-	while(((*line) != '\0') ||
-		  ((*line) != ' ')) {
+	while(((*line) != '\0') && ((*line) != ' ')) {
 		res[i] = *line;
 		i++;
 		line++;
@@ -82,7 +89,7 @@ read_line() {
 
 	int c = getchar();
 	size_t i = 0;
-	while ((c != '\n') || (c != EOF)) {
+	while ((c != '\n') && (c != EOF)) {
 		str_buf[i] = c;
 		c = getchar();
 		i++;
@@ -98,7 +105,7 @@ read_line() {
 }
 
 struct simple_cmd *
-get_simple_cmd(char *line) {
+get_simple_cmd() {
 	struct simple_cmd *res;
 	size_t args_size = 64;
 	res = alloc_simple_cmd(args_size);
@@ -106,10 +113,8 @@ get_simple_cmd(char *line) {
 
 	size_t i = 0;
 	size_t num_of_args = 0;
-	while ((line[i] != '|') ||
-		   (line[i] != '<') ||
-		   (line[i] != '>') ||
-		   (line[i] != '\0')) {
+	while ((line[i] != '|') && (line[i] != '<') &&
+		   (line[i] != '>') && (line[i] != '\0')) {
 		size_t arg_i = 0;
 		size_t arg_size = 64;
 		char *arg = malloc(sizeof(char) * arg_size);
@@ -117,11 +122,9 @@ get_simple_cmd(char *line) {
 		char stop_symbol = ' ';
 		if (line[i] == '"') {
 			stop_symbol = '"';
-			arg[arg_i] = line[i];
-			arg_i++;
 			i++;
 		}
-		while ((line[i] != stop_symbol) || (line[i] != '\0')) {
+		while ((line[i] != stop_symbol) && (line[i] != '\0')) {
 			if (line[i] == '\\') {
 				i++;
 				if (line[i] == '"') {
@@ -162,12 +165,13 @@ get_simple_cmd(char *line) {
 }
 
 struct full_cmd *
-get_full_cmd(char *line) {
+get_full_cmd() {
 	struct full_cmd *res = malloc(sizeof(struct full_cmd));
 	assert(res);
 
 	size_t simple_cmds_size = 4;
-	struct simple_cmd *simple_cmds = malloc(sizeof(struct simple_cmd) * simple_cmds_size);
+	struct simple_cmd *simple_cmds = \
+	malloc(sizeof(struct simple_cmd) * simple_cmds_size);
 	assert(simple_cmds);
 
 	size_t simple_cmd_i = 0;
@@ -176,15 +180,16 @@ get_full_cmd(char *line) {
 		simple_cmd_i++;
 		if (simple_cmd_i == simple_cmds_size) {
 			simple_cmds_size += 4;
-			simple_cmds = realloc(simple_cmds, sizeof(struct simple_cmd) * simple_cmds_size);
+			simple_cmds = \
+			realloc(simple_cmds, sizeof(struct simple_cmd) * simple_cmds_size);
 			assert(simple_cmds);
 		}
 		if ((*line) == '|') {
 			line += 2;
 			continue;
 		}
-	} while (((*line) != '\0') ||
-			 ((*line) != '<') ||
+	} while (((*line) != '\0') &&
+			 ((*line) != '<') &&
 			 ((*line) != '>'));
 	res->_simple_cmds = simple_cmds;
 	res->_num_of_simple_cmds = simple_cmd_i;
@@ -193,7 +198,9 @@ get_full_cmd(char *line) {
 		line++;
 		if ((*line) == '>') {
 			line++;
-		}
+			res->_output_flag = true;
+		} else
+			res->_output_flag = false;
 		line++;
 
 		res->_input_file = get_str(line);
@@ -202,12 +209,30 @@ get_full_cmd(char *line) {
 	if ((*line) == '<') {
 		line++;
 		if ((*line) == '<') {
-			line++;
+			line += 2;
+			char *terminator = get_str();
+			int temp_doc = open("temp_doc.txt", O_CREAT | O_WRONLY);
+			res->_input_file = "temp_doc.txt";
+			res->_input_flag = true;
+			printf("> ");
+			char *temp_str = read_line();
+			while (strcmp(terminator, temp_str)) {
+				write(temp_doc, temp_str, sizeof(temp_str));
+				write(temp_doc, "\n", 1);
+				free(temp_str);
+				printf("> ");
+				temp_str = read_line();
+			}
+			write(temp_doc, EOF, 1);
+			free(temp_str);
+			close(temp_doc);
+		} else {
+			res->_input_file = get_str();
+			res->_input_flag = false;
 		}
 		line++;
-
-		res->_input_file = get_str(line);
 	}
+
 
 	return res;
 }
@@ -230,7 +255,12 @@ launch(struct full_cmd *full_cmd) {
 		close(fdin);
 		if ((full_cmd->_num_of_simple_cmds - 1) == i) {
 			if (full_cmd->_output_file)
-				fdout = open(full_cmd->_output_file, O_CREAT | O_WRONLY);
+				if (full_cmd->_output_flag)
+					fdout = open(full_cmd->_output_file,
+								 O_CREAT | O_WRONLY | O_APPEND);
+				else
+					fdout = open(full_cmd->_output_file,
+								 O_CREAT | O_WRONLY);
 			else
 				fdout = dup(tmp_out);
 		} else {
@@ -252,6 +282,8 @@ launch(struct full_cmd *full_cmd) {
 		}
 	}
 
+	if (full_cmd->_input_flag)
+		unlink(full_cmd->_input_file);
 	dup2(tmp_in, 0);
 	dup2(tmp_out, 1);
 	close(tmp_in);
@@ -263,78 +295,50 @@ launch(struct full_cmd *full_cmd) {
 	return 1;
 }
 
-size_t number_of_builtins = 3;
-
-int
-cd(struct full_cmd *full_cmd) {
-	if (!full_cmd->_simple_cmds->_cmd_args[1]) {
-		full_cmd->_simple_cmds->_cmd_args[1] = "~\0";
-	} else
-		if (chdir(full_cmd->_simple_cmds->_cmd_args[1]))
-			perror("Error");
-	return 1;
-}
-
-int
-exit(struct full_cmd *full_cmd)
-{
-	return 0;
-}
-
-char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit",
-	NULL
-};
-
-int (*builtin_func[]) (char **) = {
-	&cd,
-	&help,
-	&exit,
-	NULL
-};
-
-int
-help(struct full_cmd *full_cmd)
-{
-	printf("Type command and press \"Enter\".\n");
-	printf("Builtins:\n");
-
-	for (size_t i = 0; i < number_of_builtins; i++)
-		printf("  %s\n", builtin_str[i]);
-	printf("Type \"man\" for another info.\n");
-	return 1;
-}
-
 int
 execute(struct full_cmd *full_cmd)
 {
 	if (full_cmd->_simple_cmds->_cmd_args == NULL)
 		return 1;
 
-	for (size_t i = 0; i < number_of_builtins; i++)
-		if (!strcmp(full_cmd->_simple_cmds->_cmd_args[0], builtin_str[i]))
-			return (*builtin_func[i])(full_cmd);
+	if (!strcmp(full_cmd->_simple_cmds->_cmd_args[0], "cd")) {
+		if (!full_cmd->_simple_cmds->_cmd_args[1]) {
+			full_cmd->_simple_cmds->_cmd_args[1] = "~\0";
+		} else
+			if (chdir(full_cmd->_simple_cmds->_cmd_args[1]))
+				perror("Error");
+		return 1;
+	}
+	else if (!strcmp(full_cmd->_simple_cmds->_cmd_args[0], "help")) {
+		printf("Type command and press \"Enter\".\n");
+		printf("Builtins:\n");
+
+		for (size_t i = 0; i < number_of_builtins; i++)
+			printf("  %s\n", builtin_str[i]);
+		printf("Type \"man\" for another info.\n");
+		return 1;
+	}
+	else if (!strcmp(full_cmd->_simple_cmds->_cmd_args[0], "exit"))
+		return 0;
 
 	return launch(full_cmd);
 }
 
 int
 main() {
-	char *line = NULL;
 	struct full_cmd* full_cmd = NULL;
 	int status;
 
 	do {
-		while (*line != '\0')
-		printf("> ");
+		printf("$ ");
 		line = read_line();
 		char *pointer = line;
 		full_cmd = get_full_cmd(line);
 		status = execute(full_cmd);
 
 		free(pointer);
-		free(full_cmd);
+		free_full_cmd(full_cmd);
 	} while (status);
+
+return 0;
 }
